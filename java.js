01f42ab9aa525af5df67cc841d35918efa8e8adf
sahiba <25sahiba03@gmail.com>
2025-04-06 19:38:57 +0530

    costom-picture

---
+++ java.js
@@ -0,0 +1,13 @@
+// var e=5;
+// let n=7;
+// const pi=3.14;
+// document.write("hellow sahiba")
+// document.write("sahiba")
+// console.log(n);
+var n;
+if(n==5){
+    alert("alert sahiba!!!");
+}
+else{
+    alert("sorry");
+}
